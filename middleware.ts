1b059fc6527b7778a4f63e51527c32ceb892b1d7
Oleksiy <Unialex@ukr.net>
2025-11-17 00:00:04 +0200

    imp: add middleware for restrict accsess to private routes without sign-in

---
+++ middleware.ts
@@ -0,0 +1,81 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { checkServerSession } from "./lib/api/serverApi";
+import { parse } from "cookie";
+
+const privateRoutes = ["/profile", "/notes"];
+const authRoutes = ["/sign-in", "/sign-up"];
+
+export const middleware = async (request: NextRequest) => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  // Шлях, на який користувач намагається перейти
+  const { pathname } = request.nextUrl;
+
+  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
+  const isPrivateRoute = privateRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (!accessToken) {
+    if (refreshToken) {
+      const data = await checkServerSession(); //refresh session
+      const setCookie = data.headers["set-cookie"];
+
+      if (setCookie) {
+        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+        for (const cookieStr of cookieArray) {
+          const parsed = parse(cookieStr);
+          const options = {
+            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+            path: parsed.Path,
+            maxAge: Number(parsed["Max-Age"]),
+          };
+          if (parsed.accessToken)
+            cookieStore.set("accessToken", parsed.accessToken, options);
+          if (parsed.refreshToken)
+            cookieStore.set("refreshToken", parsed.refreshToken, options);
+        }
+
+        // Якщо сесія все ще активна:
+        // для auth маршруту — виконуємо редірект на головну.
+        if (isAuthRoute) {
+          return NextResponse.redirect(new URL("/", request.url), {
+            headers: {
+              Cookie: cookieStore.toString(),
+            },
+          });
+        }
+        // для приватного маршруту — дозволяємо доступ
+        if (isPrivateRoute) {
+          return NextResponse.next({
+            headers: {
+              Cookie: cookieStore.toString(),
+            },
+          });
+        }
+      }
+    }
+    // Якщо refreshToken або сесії немає:
+    // приватний маршрут — редірект на сторінку входу
+    if (isPrivateRoute) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+    //всі іньші пропускаємо
+    return NextResponse.next();
+  }
+
+  // is public route або accessToken є
+  // приватний маршрут — виконуємо редірект на головну
+  if (isAuthRoute) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  //всі іньші пропускаємо
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/notes:path*", "/profile/:path*", "/sign-in", "/sign-up"],
+};
